97a14eecc456090ef107a96b8cfc7f36b4f0fad8
Thayná Luiza Gitirana da Cunha <61708182+gitirana@users.noreply.github.com>
2020-10-15 16:30:27 -0300

    Changing the map theme

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -48,7 +48,7 @@ function OrphanagesMap() {
         style={{ width: '100%', height: '100%' }}
       >
 
-      <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/navigation-guidance-night-v4/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+      <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/navigation-guidance-day-v4/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
 
         {orphanages.map(orphanage => {
           return (
